1111715632e8261bb5fa075221c1ef2733886775
Endy Jasmi <endyjasmi@gmail.com>
2021-02-27 11:07:29 +0800

    feat(@marubase/collator): implement multi item append support

--- packages/collator/base-coder.ts
+++ packages/collator/base-coder.ts
@@ -3,20 +3,33 @@ import { CodeTable } from "./code-table";
 export abstract class BaseCoder {
   public constructor(public table: typeof CodeTable) {}
 
-  public append(binary: Uint8Array, bytes: Uint8Array): Uint8Array {
+  public append(binary: Uint8Array, ...items: Uint8Array[]): Uint8Array {
     let buffer = new Uint8Array(binary.buffer);
-    while (binary.byteOffset + bytes.length > buffer.length) {
+
+    let itemByteLength = 0;
+    const itemLength = items.length;
+    for (let index = 0; index < itemLength; index++) {
+      itemByteLength += items[index].length;
+    }
+
+    while (binary.byteOffset + itemByteLength > buffer.length) {
       let newLength = buffer.length * 2 || 1;
-      while (binary.byteOffset + bytes.length > newLength) newLength *= 2;
+      while (binary.byteOffset + itemByteLength > newLength) newLength *= 2;
       const newBuffer = new Uint8Array(newLength);
       newBuffer.set(buffer);
       buffer = newBuffer;
     }
-    buffer.set(bytes, binary.byteOffset);
+
+    let itemOffset = binary.byteOffset;
+    for (let index = 0; index < itemLength; index++) {
+      buffer.set(items[index], itemOffset);
+      itemOffset += items[index].length;
+    }
+
     return new Uint8Array(
       buffer.buffer,
       binary.byteOffset,
-      binary.length + bytes.length
+      binary.length + itemByteLength
     );
   }
 }
