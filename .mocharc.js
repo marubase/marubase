bc9b76a2d4de1cef0851a6c991c0a0bf56408742
Endy Jasmi <endyjasmi@gmail.com>
2021-02-28 20:56:39 +0800

    build: update mocha and nyc reporter

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
   extension: ["js", "jsx", "ts", "tsx"],
-  reporter: ["spec"],
+  reporter: process.env.NODE_ENV !== "test" ? ["spec"] : ["min"],
   require: ["ts-node/register", "source-map-support/register"],
 };
--- nyc.config.js
+++ nyc.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   exclude: ["**/*.spec.ts", "**/*.spec-helper.ts"],
   extends: "@istanbuljs/nyc-config-typescript",
-  reporter: ["html", "lcov", "text"],
+  reporter:
+    process.env.NODE_ENV !== "test" ? ["html", "text"] : ["lcovonly", "text"],
 };
