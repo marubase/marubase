42d1242f0684a221c0e6391fc78f517178f0685d
Endy Jasmi <endyjasmi@gmail.com>
2021-03-02 15:33:23 +0800

    fix(@marubase/contract): remove meta value from value contract

--- packages/contract/collator/value.contract.ts
+++ packages/contract/collator/value.contract.ts
@@ -1,9 +1,6 @@
-import { MetaValueContract } from "./meta-value.contract";
-
 export type ValueContract =
   | { [key: string]: ValueContract }
   | Date
-  | MetaValueContract
   | Uint8Array
   | ValueContract[]
   | boolean
